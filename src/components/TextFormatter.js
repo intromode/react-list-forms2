6a7c36fe81f2f465a8bf67d00bc25af0b853882d
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-05-15 15:14:59 -0700

    blank init

--- src/components/TextFormatter.js
+++
@@ -1,7 +0,0 @@
-//create a textFormatter component:
-//use a text input box to capture the text
-//print the text below the input 
-//use a color input box to set the color of the text 
-
-import React, { PureComponent } from 'react';
-
