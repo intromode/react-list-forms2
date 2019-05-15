1834e9d76ed76b914448540693a881a4bf5f154f
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-05-15 14:09:47 -0700

    init commit with components folder and textFormatter, before i add more

---
+++ src/components/TextFormatter.js
@@ -0,0 +1,7 @@
+//create a textFormatter component:
+//use a text input box to capture the text
+//print the text below the input 
+//use a color input box to set the color of the text 
+
+import React, { PureComponent } from 'react';
+
